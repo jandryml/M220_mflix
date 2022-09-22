30917149427b4ba23d40085ebd089f1e07291d00
Jan Dryml <jan.dryml@etnetera.cz>
2022-09-22 14:38:51 +0200

    ch1 - ticket: Projection

--- src/main/java/mflix/api/daos/MovieDao.java
+++ src/main/java/mflix/api/daos/MovieDao.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+import static com.mongodb.client.model.Filters.all;
+
 @Component
 public class MovieDao extends AbstractMFlixDao {
 
@@ -118,10 +120,13 @@ public class MovieDao extends AbstractMFlixDao {
      */
     public List<Document> getMoviesByCountry(String... country) {
 
-        Bson queryFilter = new Document();
-        Bson projection = new Document();
-        //TODO> Ticket: Projection - implement the query and projection required by the unit test
+        Bson queryFilter = all("countries", country);
+        Bson projection = new Document("title", 1);
         List<Document> movies = new ArrayList<>();
+        moviesCollection
+                .find(queryFilter)
+                .projection(projection)
+                .into(movies);
 
         return movies;
     }
